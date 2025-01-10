d3e995f5de6a0bbbab1a76eecb08ca827c814039
Mostafa Salem <mostafa.salem.ext@siemens.com>
2025-01-10 20:14:53 +0200

    added limits values as configuration properties and injected them to the controller

--- src/main/java/com/example/limitsservice/controller/LimitsController.java
+++ src/main/java/com/example/limitsservice/controller/LimitsController.java
@@ -1,14 +1,19 @@
 package com.example.limitsservice.controller;
 
 import com.example.limitsservice.bean.Limits;
+import com.example.limitsservice.configuration.LimitsConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
+    @Autowired
+    private LimitsConfiguration configuration;
+
     @GetMapping("/limits")
     public Limits retrieveLimits() {
-        return new Limits(1, 1_000_000);
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
     }
 }
